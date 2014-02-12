cbaa6ca6c11b7b232725119b6f53010326deeeca
Vegard Stene <jvstene@gmail.com>
2014-02-12 15:02:37 +0100

    Add compass config file

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "src/css"
+sass_dir = "src/scss"
+images_dir = "src/img"
+javascripts_dir = "src/js"
+
+output_style = :nested
+environment = :development
+
+relative_assets = true
